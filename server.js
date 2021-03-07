5fa9a761caaa018052d8d74f6315398622a89e05
Ray Luna <leon.luna.ray@gmail.com>
2021-03-07 00:11:38 -0800

    server loading index

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+// const db = require('./models');`
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.sendFile('views/index.html');
+    res.sendFile(path.join(__dirname + '/views/index.html'));
 });
 
-app.listen(3000, () => console.log(` 
+app.listen(PORT, () => console.log(` 
  🤖 The server is listening on port ${PORT}
 `));
+
+// Sequelize add later
+
+// db.sequelize.sync().then(() => {
+//     app.listen(PORT, () => {
+//       console.log(
+//         '==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.',
+//         PORT,
+//         PORT,
+//       );
+//     });
+//   });
