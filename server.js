49c42808f58a1df3e3ddb02c8fa7edb294615a77
Ray Luna <leon.luna.ray@gmail.com>
2021-03-06 22:00:54 -0800

    update get route

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html');
+    res.sendFile('views/index.html');
 });
 
 app.listen(3000, () => console.log(` 
