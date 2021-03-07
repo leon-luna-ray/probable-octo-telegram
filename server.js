21b8effd34c82c95a4c3ec17e0bbcc3c26f075e5
Ray Luna <leon.luna.ray@gmail.com>
2021-03-06 21:56:40 -0800

    update get route

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const app = express();
 
 const PORT = 3000;
 
-// app.get('/', (req, res) => {
-//     res.send('An alligator approaches!');
-// });
+app.get('/', (req, res) => {
+    res.sendFile('./views/index.html');
+});
 
-app.listen(3000, () => console.log(` 🤖 The server is listening on port ${PORT}`));
+app.listen(3000, () => console.log(` 
+ 🤖 The server is listening on port ${PORT}
+`));
