67e208def981c6c4b98ad2efc91716b2bdd2caa3
Ray Luna <leon.luna.ray@gmail.com>
2021-03-07 21:46:32 -0800

    update frontend

--- public/js/mood.js
+++ public/js/mood.js
@@ -1,37 +1,34 @@
+// const moodsArr = ['afraid', 'angry', 'annoyed', 'anxious', 'bored', 'calm', 'confused', 'crazy ', 'disgusted', 'embarrassed', 'excited', 'happy ', 'lonely', 'nostalgic', 'numb', 'optimistic', 'proud', 'sad', 'sick', 'silly', 'sleepy', 'stressed', 'surprised','tired'];
+
 // mood = {
 //     afraid: document.getElementById('afraid'),
-//     // angry
-//     // annoyed
-//     // anxious
-//     // bored
-//     // calm
-//     // confused
-//     // crazy 
-//     // disgusted
-//     // embarrassed
-//     // excited
-//     // happy 
-//     // lonely
-//     // nostalgic
-//     // numb
-//     // optimistic
-//     // proud
-//     // sad
-//     // sick
-//     // silly
-//     // sleepy
-//     // stressed
-//     // surprised
-//     // tired
-
+//     angry: document.getElementById('afraid'),
+//     annoyed: document.getElementById('afraid'),
+//     anxious: document.getElementById('afraid'),
+//     bored: document.getElementById('afraid'),
+//     calm: document.getElementById('afraid'),
+//     confused: document.getElementById('afraid'),
+//     crazy: document.getElementById('afraid'),
+//     disgusted: document.getElementById('afraid'),
+//     embarrassed: document.getElementById('afraid'),
+//     excited: document.getElementById('afraid'),
+//     happy: document.getElementById('afraid'),
+//     lonely: document.getElementById('afraid'),
+//     nostalgic: document.getElementById('afraid'),
+//     numb: document.getElementById('afraid'),
+//     optimistic: document.getElementById('afraid'),
+//     proud: document.getElementById('afraid'),
+//     sad: document.getElementById('afraid'),
+//     sick: document.getElementById('afraid'),
+//     silly: document.getElementById('afraid'),
+//     sleepy: document.getElementById('afraid'),
+//     stressed: document.getElementById('afraid'),
+//     surprised: document.getElementById('afraid'),
+//     tired: document.getElementById('afraid'),
 // };
 
-// mood.afraid.addEventListener('click', event => {
-//     console.log('afraid!')
-//   });
+const moodBtn = document.querySelector('.mood-btn');
 
-const happy = document.getElementById('happy');
-
-happy.addEventListener('click', () =>{
-    console.log('happy!')
-})
+moodBtn.addEventListener('click', () => {
+    console.log('clicked')
+});
