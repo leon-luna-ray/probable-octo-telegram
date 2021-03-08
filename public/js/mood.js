9f7e97c3df8c9e64c13160950888e81743ed021f
Ray Luna <leon.luna.ray@gmail.com>
2021-03-07 23:01:46 -0800

    update

--- public/js/mood.js
+++ public/js/mood.js
@@ -1,34 +1,5 @@
-// const moodsArr = ['afraid', 'angry', 'annoyed', 'anxious', 'bored', 'calm', 'confused', 'crazy ', 'disgusted', 'embarrassed', 'excited', 'happy ', 'lonely', 'nostalgic', 'numb', 'optimistic', 'proud', 'sad', 'sick', 'silly', 'sleepy', 'stressed', 'surprised','tired'];
 
-// mood = {
-//     afraid: document.getElementById('afraid'),
-//     angry: document.getElementById('afraid'),
-//     annoyed: document.getElementById('afraid'),
-//     anxious: document.getElementById('afraid'),
-//     bored: document.getElementById('afraid'),
-//     calm: document.getElementById('afraid'),
-//     confused: document.getElementById('afraid'),
-//     crazy: document.getElementById('afraid'),
-//     disgusted: document.getElementById('afraid'),
-//     embarrassed: document.getElementById('afraid'),
-//     excited: document.getElementById('afraid'),
-//     happy: document.getElementById('afraid'),
-//     lonely: document.getElementById('afraid'),
-//     nostalgic: document.getElementById('afraid'),
-//     numb: document.getElementById('afraid'),
-//     optimistic: document.getElementById('afraid'),
-//     proud: document.getElementById('afraid'),
-//     sad: document.getElementById('afraid'),
-//     sick: document.getElementById('afraid'),
-//     silly: document.getElementById('afraid'),
-//     sleepy: document.getElementById('afraid'),
-//     stressed: document.getElementById('afraid'),
-//     surprised: document.getElementById('afraid'),
-//     tired: document.getElementById('afraid'),
-// };
 
-const moodBtn = document.querySelector('.mood-btn');
-
-moodBtn.addEventListener('click', () => {
-    console.log('clicked')
-});
+function addMood (mood) {
+    console.log(mood);
+};
