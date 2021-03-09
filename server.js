70f590c93d0055f593d98b8bad06b8a3a7ebf71e
Ray Luna <leon.luna.ray@gmail.com>
2021-03-08 20:21:46 -0800

    db connection successful" "

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
-const app = express();
 const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 const db = require('./models');
 
 // Serve static content for the app from the public directory, body parser for front end
+const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+// // Routes set up once server running again
+// require("./routes/html-routes.js")(app);
+// require("./routes/api-routes.js")(app);
+
+// move to html routes
 
 // use unit 14 hw develop folder to change route based on authentication, and move to seprate routes file.
 app.get('/', (req, res) => {
